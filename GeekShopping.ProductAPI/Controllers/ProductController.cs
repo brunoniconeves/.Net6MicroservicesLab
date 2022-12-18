68aa5b0f27dc5ce18db4fdc5ce213d7228797c51
Bruno Bastos Neves <brunoniconeves@gmail.com>
2022-12-17 22:43:22 -0300

    implement create, update and delete at controller

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -32,11 +32,40 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpGet("{id}")]
-        //find product by ID
         public async Task<ActionResult> FindById(long id){
             var product = await _repository.FindById(id);
             if (product == null) return NotFound();
             return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ProductVO productVO)
+        {
+            if (productVO == null) return BadRequest();  
+            var product = await _repository.Create(productVO);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(ProductVO productVO)
+        {
+            if (productVO == null) return BadRequest();
+            ProductVO productFound = await _repository.FindById(productVO.Id);
+
+            if(productFound == null) return BadRequest();
+
+            ProductVO product = await _repository.Update(productVO);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            bool status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return Ok(status);
         }
     }
 }

c4b15363ce3be5d27d1cccc0d21471322c734827
Bruno Bastos Neves <brunoniconeves@gmail.com>
2022-12-19 23:37:32 -0300

    Web App product models and service interface

---
+++ GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAll();
+        Task<ProductModel> FindById(long id);
+        Task<ProductModel> Create(ProductModel product);
+        Task<ProductModel> Update(ProductModel product);
+        Task<bool> DeleteById(long id);
+    }
+}

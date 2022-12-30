36f61719919769292c276dc849ce3c4e300351ad
Bruno Bastos Neves <brunoniconeves@gmail.com>
2022-12-30 02:38:36 -0300

    Configure IdentityServer

---
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace GeekShopping.IdentityServer.Configuration
+{
+  public static class IdentityConfiguration
+  {
+      public const string Admin = "Admin";
+      public const string Customer = "Customer";
+
+      public static IEnumerable<IdentityResource> IdentityResources =>
+          new List<IdentityResource>
+          {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile(),
+            new IdentityResource("roles", "Your role(s)", new List<string>() {"role"}),
+            new IdentityResource("location", "Your location", new List<string>() {"location"})
+          };
+
+      public static IEnumerable<ApiScope> ApiScopes => 
+          new List<ApiScope>
+          {
+              new ApiScope("geek_shopping", "GeekShopping Server"),
+              new ApiScope("read", "Read data."),
+              new ApiScope("write", "Write data."),
+              new ApiScope("delete", "Delete data."),
+          };
+
+      public static IEnumerable<Client> Clients =>
+          new List<Client>
+          {
+              new Client {
+                  ClientId = "client",
+                  ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                  AllowedGrantTypes = GrantTypes.ClientCredentials,
+                  AllowedScopes = {"read", "write", "profile","geek_shopping" }
+              }, 
+              new Client {
+                  ClientId = "geek_shopping",
+                  ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                  AllowedGrantTypes = GrantTypes.Code,
+                  RedirectUris = {"http://localhost:8440/signin-oidc"},
+                  PostLogoutRedirectUris = {"http://localhost:8440/signout-callback-oidc"},
+                  AllowedScopes = new List<string>
+                  {
+                      IdentityServerConstants.StandardScopes.OpenId,
+                      IdentityServerConstants.StandardScopes.Email,
+                      IdentityServerConstants.StandardScopes.Profile,
+                      "geek_shopping"
+                  }
+              }        
+          };    
+
+      
+
+
+  }
+}
--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,44 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection,
+        new MySqlServerVersion(
+            new Version(5, 7, 32)
+        )
+    )
+);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(options =>
+    {
+      options.Events.RaiseErrorEvents = true;
+      options.Events.RaiseInformationEvents = true;
+      options.Events.RaiseFailureEvents = true;
+      options.Events.RaiseSuccessEvents = true;
+      options.EmitStaticAudienceClaim = true;
+    }
+).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+builder.Services.AddDeveloperSigningCredential();
+
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
